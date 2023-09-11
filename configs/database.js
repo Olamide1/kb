97b814024b53ff5b35b80ea358989034ca93cb89
Ossai Nwachukwu <nwachukwuossai@gmail.com>
2023-09-11 08:29:37 +0100

    updated prod db env name

--- configs/database.js
+++ configs/database.js
@@ -6,12 +6,12 @@ const { Sequelize } = require('sequelize')
 
 module.exports = {
     production: {
-        username: process.env.SHARED_HOST_DB_USERNAME,
-        password: process.env.SHARED_HOST_DB_PASSWORD,
-        database: process.env.SHARED_HOST_DB_NAME,
-        host: process.env.SHARED_HOST,
-        port: process.env.SHARED_HOST_DB_PORT,
-        dialect: process.env.SHARED_HOST_DB_DIALECT,
+        username: process.env.PROD_DB_USERNAME,
+        password: process.env.PROD_DB_PASSWORD,
+        database: process.env.PROD_DB_NAME,
+        host: process.env.PROD_DB_HOST,
+        port: process.env.PROD_DB_PORT,
+        dialect: process.env.PROD_DB_DIALECT,
         dialectOptions: {
             bigNumberStrings: true,
             //   ssl: {
